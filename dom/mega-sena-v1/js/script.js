8e98e3a5924c8086da744772f123922b1b10e082
Eduardo <dudu_jr_007@hotmail.com>
2022-08-12 20:06:57 -0300

    criando funções para remover números do jogo, verificar se o número escolhido está no jogo, verificar se o jogo está completo e salvar o jogo atual

--- dom/mega-sena-v1/js/script.js
+++ dom/mega-sena-v1/js/script.js
@@ -1,16 +1,21 @@
 var state = { board: [], currentGame: [], savedGames: [] };
 
-function start () 
+function start() 
 {
-    addNumberToGame = 1;
-    addNumberToGame = 2;
-    addNumberToGame = 3;
-    addNumberToGame = 4;
-    addNumberToGame = 5;
-    addNumberToGame = 61;
-    addNumberToGame = -3;
+    addNumberToGame(1);
+    addNumberToGame(2);
+    addNumberToGame(3);
+    addNumberToGame(4);
+    addNumberToGame(5);
+    saveGame();
+    addNumberToGame(6);
+
+    saveGame();
+    saveGame();
+
     
     console.log(state.currentGame);
+    console.log(state.savedGames);
 }
 
 function addNumberToGame(numberToAdd)
@@ -25,7 +30,64 @@ function addNumberToGame(numberToAdd)
         console.error("O jogo já está completo");
         return;
     }
+    if (isNumberInGame(numberToAdd))
+    {
+        console.error("Este número já está no jogo", numberToAdd)
+        return;
+    }
     state.currentGame.push(numberToAdd);
+}
+
+function removeNumberFromGame(numberToRemove)
+{
+    if (numberToRemove < 1 || numberToRemove > 60)
+        {
+            console.error("Número inválido", numberToRemove);
+            return;
+        }
+
+    var newGame = [];
+
+    for (var i = 0; i < state.currentGame.length; i++)
+    {
+        var currentNumber = state.currentGame[i];
+
+        if (currentNumber === numberToRemove)
+        {
+            continue;
+        }
+
+        newGame.push(currentNumber);
+    }
+
+    state.currentGame = newGame;
+}
+
+function isNumberInGame(numberToCheck)
+{
+    /* if (state.currentGame.includes(numberToCheck))
+    {
+        return true;
+    }
+    return false; */
+
+    return state.currentGame.includes(numberToCheck);
+}
+
+function saveGame()
+{
+    if (!isGameComplete())
+    {
+        console.error("O jogo não está completo!");
+        return;
+    }
+
+    state.savedGames.push(state.currentGame);
+}
+
+function isGameComplete()
+{
+    return state.currentGame.length === 6;
 }
 
 start();
